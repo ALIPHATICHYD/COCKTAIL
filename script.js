db892829f4a4c525a1a785a2313a64b6f3f40124
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-06-18 00:42:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,634 @@
+//console.log('ALIPHATIC');
+
+//var number = 5;
+//    number = 5 * 10;
+
+//    console.log(number);
+
+//let number = 5;
+ //number = 5 * 10;
+ //console.log(number);
+
+ //   const number = 5;
+ //number = 5 * 10;
+
+// console.log(number);
+
+/*let number1 = prompt('what is the first number ');
+
+let number2 = prompt('what is the second number'); 
+
+let result = Number(number1) + Number(number2);
+
+
+alert(result);*/
+
+/*let First = prompt('What is your first Name');
+
+let Second = prompt('What is your second Name');
+
+let Last = prompt('What is your Third Name');
+
+let result = First + Second + Last;
+
+alert(result);*/
+
+//console.log([2, 3, 4])
+
+/*const arr = [5, 7 , 7, 10];
+
+arr.pop();
+
+console.log(arr)
+
+/*const num =10;
+num = 20;
+
+console.log(num);*/
+
+/*const name1= 'samuel';
+console.log(name1.toUpperCase())*/
+//concatenation
+/*const name1 = ' samuel';
+const pet = ' dog';
+const lastname = ' oketola ';
+
+let statement = 'HI, I am' + name1 + lastname + 'I have a' + pet;
+
+console.log(statement);*/
+
+/*const myname = 'ALIPHATIC';
+const age = '';
+const tstatus = 'Full Stack Web Dev';
+
+let statement = 'Hi, my name is ' + myname + ' I am ' + age + ' I am studying ' + tstatus + ' at CBC';
+
+console.log(statement);*/
+
+//Convert To String
+//String function
+
+/*let num1 = String(6) + String(6);
+let date = String(new Date());
+let bool = String(true)
+
+console.log(typeof bool);
+console.log(typeof num1);
+console.log(num1)*/
+
+/*let num1 = String(5);
+let num2 = String(5);
+
+console.log(num1 * num2);*/
+
+/*let num1 = 7;
+let num2 = String(7);
+
+console.log(num1 + num2);*/
+
+//Number Conversion
+
+//let num1 = Number('6') + Number('8');
+//let date = String (new Date());
+
+// let num1 = 5;
+// let num2 = "34";
+
+// console.log(num1 + num2);
+
+// let num3 = 5;
+// let num4 = "4";
+
+// console.log(num3 - num4);
+
+// let num5 =  10;
+// let num6 =  5;
+
+// console.log(num5 % num6);
+
+// let num7 =  5;
+// let num8 =  10;
+
+// console.log(num7 % num8);
+
+// let num9 =  "Java";
+// let num10 =  "Script";
+
+// console.log(num9 + num10);
+
+// let num11 =  "";
+// let num12 =  "";
+
+// console.log(num11 + num12);
+
+// let num13 =  "";
+// let num14 =  0;
+
+// console.log(num13 + num14);
+
+// let num15 =  true;
+// let num16 =  true;
+
+// console.log(num15 + num16);
+
+// let num17 =  true;
+// let num18 =  false;
+
+// console.log(num17 + num18);
+
+// let num19 =  false;
+// let num20 =  true;
+
+// console.log(num19 + num20);
+
+// let num21 =  false;
+// let num22 =  true;
+
+// console.log(num21 - num22);
+
+// let num23 =  3;
+// let num24 =  4;
+
+// console.log(num23 - num24);
+
+// let num25 =  "Bob";
+// let num26 =  "Bill";
+
+// console.log(num25 - num26);
+
+// let firstn =' ALIPHA';
+// let lastn = 'TIC';
+
+// let result = firstn + lastn;
+
+// console.log(result);
+
+// let numa =  34;
+// let numb =  21;
+// a = 2;
+
+// console.log(numa + numb);
+
+// var c;
+// console.log(c);
+
+//String Method
+
+// const fName = 'Benedict';
+// const statement= 'Hey how are you'
+// const lName = 'Doe';
+// let val;
+
+// val = fName.concat('',lName);
+
+// val = fName.toUpperCase();
+
+
+// //index of
+// val = fName .indexOf('n');
+
+// //Substring
+// val =fName.substring(0,3)
+
+
+// //Slice
+// val = fName.slice(-2);
+
+// //Char at
+// val = fName.charAt(1);
+
+// //split
+// val = statement.split(' ');
+
+// //includes
+// val = statement.includes('is')
+
+// //replace
+// val = statement.replace('are', 'is')
+
+
+// let val;
+
+// val = Math.PI;
+
+// let evaluate;
+
+// //round
+// evaluate = Math.round(val);
+
+// //ceil
+// evaluate = Math.ceil(val)
+
+// //floor
+// evaluate = Math.floor(3.9)
+
+// //sqrt
+// evaluate = Math.sqrt(4)
+
+// //abs
+// evaluate = Math.abs(-50)
+
+// //power
+// evaluate = Math.pow(6, 2);
+
+// //min
+// evaluate = Math.min(2, 3 , 7, 1);
+
+// //max
+// evaluate = Math.max(2.3, 8.4, 0.5 )
+
+// //math random
+// evaluate = Math.random()*10;
+
+// //
+// evaluate = Math.floor(Math.random()*20 )
+
+// console.log(evaluate);
+
+//Evaluate the below comparison:
+
+// console.log(5 >= 1);
+
+// console.log(0 === 1);
+
+// console.log(4 <= 1);
+
+// console.log(1 != 1);
+
+// console.log("A" > "B");
+
+// console.log("A" < "C");
+
+// console.log("a" > "A");
+
+// console.log("b" < "A");
+
+// console.log(true === false);
+
+// console.log(true != true); 
+
+
+//IF /ELSE STATEMENTS
+
+// const id = '1';
+
+// if(id === 1){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// const color = 'green';
+
+// if(color == 'red'){
+//     console.log('stop');
+// }else if(color === 'amber'){
+//     console.log('Go');
+// }else if (color === 'green'){
+
+// }else{
+//     console.error('Your color is not a value');
+// }
+
+
+
+// //AGE LIMIT TASK
+// let age= Number(prompt('How old are you'));
+
+// if (age == NaN(alert(Sorry)));
+
+
+
+// switch (color){
+
+//     case'red':
+
+//     console.log('Stop');
+//     break; 
+
+//     case'amber':
+
+//     console.log('Ready');
+//     break;
+    
+//     case'green':
+
+//     console.log('Go');
+//     break; 
+
+// default :
+// console.error ('Your color is not a valid tr');
+
+// break;
+// }
+
+// Program for cocktails
+
+// const cocktail = prompt('What is your favourite cocktail')
+
+// switch(cocktail){
+
+// case'Martini':
+
+// alert('Senior man choice!');
+// break;
+
+// case'Daiquiri':
+
+// alert('Good Taste!');
+// break;
+
+// case'Magarita':
+
+// alert('Why so soft?');
+// break;
+
+// case'default':
+
+// alert('We dont have that here');
+// break;
+
+// }
+
+// Array
+
+// const arr1 = [3200, 400, 130000, 29, 6];
+// const arr2 = new Array('fish', 'meat', 32, 45, 'mutton');
+// const arr3 = {
+//     0: 1,
+//     1: 50,
+//     2: 300
+// }
+// console.table(arr2);
+// console.log(arr2.indexOf(32));
+// //console.table(Aray.isArray(arr3));
+
+//Mutating arrays
+//removing from end:
+// arr2.pop()
+// console.table(arr2);
+
+// push to end
+// arr2.push('goat')
+// console.table(arr2);
+
+
+// removing from start:
+// arr2.shift();
+// console.tablea(arr2);
+
+// splice:
+// arr2.splice(1, 3);
+// console.log(arr2);
+
+//Reverse
+// arr2.reverse();
+// console.log(arr2);
+
+//Joining Arrays
+// console.log(arr1.concat(arr2));
+
+// //sort arrays
+// console.log(arr1.sort(function(x, y){
+//     return y - x;
+// }));
+
+//find
+// function below10(num){
+//     return num < 10;
+
+// };
+
+// console.log(arr1.find(below10));
+
+// Date and Time
+// const today = new Date();
+// console.log(today);
+
+// const birthday = new Date('8-15-2007 11:25:50')
+// let val;
+//  val = birthday.getDate();
+//  val = birthday.getDay();
+//  val = birthday.getMonth();
+//  val = birthday.getFullYear();
+//  val = birthday.getHours();
+//  val = birthday.getMinutes();
+//  val = birthday.getSeconds();
+
+//  val = birthday.getTime();
+//  console.log(val);
+
+
+// const birthday = new Date('8-15-2007 11:25:50')
+
+//  val = birthday.getDate();
+//  val = birthday.getDay();
+//  val = birthday.getMonth();
+//  val = birthday.getFullYear();
+//  val = birthday.getHours();
+//  val = birthday.getMinutes();
+//  val = birthday.getMilliseconds();
+
+//  val = birthday.setMonth(2);
+//  val = birthday.setDate(2);
+//  val = birthday.setFullYear(2001);
+//  val = birthday.setMonth(2);
+
+// //  console.log(birthday);
+
+// const dayOfweek = new Date().getDay();
+// let day;
+
+// switch(dayOfWeek){
+//     case 0:
+//         day = 'Sunday';
+//         break;
+        
+//         case 1:
+//             day = 'Monday';
+//             break;
+    
+//             case 2:
+//                 day = 'Tuesday';
+//                 break;
+
+//                 case 3:
+//                     day = 'Wednesday';
+//                     break;
+
+//                     case 4:
+//                         day = 'Thursday';
+//                         break;
+
+//                         case 5:
+//                             day = 'Friday';
+//                             break;
+
+//                             case 6:
+//                                 day = 'Saturday';
+//                                 break;
+                                                                                                 
+
+// }
+
+// console.log(`Today is ${day}`);
+
+
+// LOOPS💀
+// Running a code multiple times in a program
+// Types Of Loops
+// 1- While Loop: This  comesbefore the condition
+// 1st parameteri;
+// infinity loop code💀
+// let i= 0
+// while (i <10) {
+// console.log(i);
+// }
+// // 1 < 5
+// i += 2💀
+
+// var myArray = [];
+// var i = 0;
+// while (i < 9) {
+//     myArray.push(i);
+//     i++
+
+// }
+// console.log(myArray)
+
+// For Loop
+// for(let i = 0; i <= 0; i++){
+//     console.log(i);
+// }
+
+//OR
+// var myArray = [];
+// for (var i = 0; i < 8; i++){
+//     myArray.push(i);
+// }
+// console.log(myArray);
+
+// for (let i = 0; i < 10; i++){
+//     if (i === 5){
+//         console.log('a break at 5');
+//         continue; 
+//     }
+//     console.log(i);
+// }
+
+// for(let i = 1; i <=7; i++){
+//     console.log(i);
+// }
+
+// Iterate odd numbers with for loops
+//  var myArray = [];
+// for (var i = 1; i < 10; i++){
+//         if (i %2 !== 0)
+//    myArray.push(i);
+  
+// }
+//   console.log(myArray);
+
+//OR THIS METHOD
+// var myArray = [];
+// for (var i = 1; i < 10; i+=2) {
+//     myArray.push(i);
+//     console.log(i);
+// }
+
+// var ourArray = []
+// for(let i = 15, i < 30; i +=2) {
+//     ourArray.push(i);
+
+// }console.log(i);
+
+
+// // Counting backward with a for loop
+// var myArray = [];
+//  for (var i = 10; i >= 0 ;  i--) {
+//      myArray.push(i);
+//    console.log(i);
+// }
+
+// Iterate Through an Array using a For Loop
+
+// var ourArray = [2, 4, 5, 6,]
+// var ourTotal = 0;
+
+// for(var i = 0; i < ourArray.length, i++){
+//     ourTotal += ourArray[i];
+// }
+// console.log(ourTotal);
+
+// exercise
+//  const myArray = [12, 13, 14, 15, 18, 19]
+//  var total = 3;
+//  for (var i = 1; i<(myArray.length); i+=2){
+//     total += myArray[1];
+//  }
+//  console.log(myArray)
+
+//Nesting For Loops
+
+// var arr = [
+//     [1,2], [3,4], [6,8]
+// ];
+
+// for(var i = 0; i < arr.length; i++){
+//     for(var j = 0; j < arr[i].length; j++) {
+//         console.log(arr[i][j]);
+//     }
+// }
+
+// const arr = [1, 2, 3, 4];
+// let total = 0;
+
+// for(let x of arr){
+//     total += x;
+// }
+// console.log(total);
+
+
+
+
+// FUNCTIONS
+
+// function addNums(num1=2 ,num2=5){
+//    return num1 + num2
+// }
+// console.log(addNums(5,10))
+
+// FUNCTION EXPRESSION
+
+// const square = function(x){
+//     return x * x; 
+// }
+// console.log(square(2));
+
+// IIFE'S
+
+// (function(name){
+//     console.log('Hello {name}');
+// }) ('Ben');
+
+const square = function(x){
+    return 'Area: {Math.PI}, Perimeter: {}';
+}
+
+
+//DO...... WHILE LOOP
+// var myArray = [];
+// var i = 10;
+
+// do{
+//     myArray.push(i);
+//     i++;
+// }while (i < 5)
+// console.log(i, myArray);
+
+
+let i = 0;
+do{
+    console.log(`Num ${i}`);
+i++;
+}while(i < 10)
